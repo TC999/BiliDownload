3a76fcf18a43ba6b0404460cc66778aec93843d2
Kafuu Neko <kafuuneko@gmail.com>
2025-06-09 00:55:23 +0800

    refactor: 优化二维码位图生成逻辑

--- app/src/main/java/cc/kafuu/bilidownload/common/jni/QrCodeNativeLib.kt
+++ app/src/main/java/cc/kafuu/bilidownload/common/jni/QrCodeNativeLib.kt
@@ -1,26 +1,32 @@
 package cc.kafuu.bilidownload.common.jni
 
 import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.graphics.Color
+import android.graphics.Paint
 import androidx.core.graphics.createBitmap
-import androidx.core.graphics.set
 
 object QrCodeNativeLib {
     fun generateQrBitmap(text: String, scale: Int = 10): Bitmap {
         val (data, width) = NativeLib.generateQrCode(text)
         val bitmapSize = width * scale
         val bitmap = createBitmap(bitmapSize, bitmapSize)
+        val canvas = Canvas(bitmap)
+        val paint = Paint()
+
         for (y in 0 until width) {
             for (x in 0 until width) {
                 val index = y * width + x
-                val color = if (data[index]) Color.BLACK else Color.WHITE
-                for (dy in 0 until scale) {
-                    for (dx in 0 until scale) {
-                        bitmap[x * scale + dx, y * scale + dy] = color
-                    }
-                }
+                paint.color = if (data[index]) Color.BLACK else Color.WHITE
+                val left = x * scale.toFloat()
+                val top = y * scale.toFloat()
+                val right = left + scale
+                val bottom = top + scale
+                canvas.drawRect(left, top, right, bottom, paint)
             }
         }
+
         return bitmap
     }
+
 }
